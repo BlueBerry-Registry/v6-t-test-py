097207d777a346442db22796d821d623bb853abf
Vittoria Ramella <vittoria.ramella@biomeris.it>
2025-02-06 12:08:05 +0100

    Added partial function

--- v6-independent-samples-t-test-py/partial.py
+++ v6-independent-samples-t-test-py/partial.py
@@ -7,25 +7,54 @@ encryption if that is enabled). From there, they are sent to the partial task
 or directly to the user (if they requested partial results).
 """
 import pandas as pd
+import pandas.api.types as ptypes
 from typing import Any
 
-from vantage6.algorithm.tools.util import info, warn, error
+from vantage6.algorithm.tools.util import info, error
 from vantage6.algorithm.tools.decorators import data
 
 
 @data(1)
 def partial(
-    df1: pd.DataFrame, arg1
+    df: pd.DataFrame,
+    col_name: str
 ) -> Any:
 
-    """ Decentral part of the algorithm """
-    # TODO this is a simple example to show you how to return something simple.
-    # Replace it by your own code
-    info("Computing mean age by gender")
-    result = df1[["Gender", "Age"]].groupby("Gender").mean()
+    """ 
+    Compute the mean and the sample variance of a column for a single data station to share with the
+    aggregator part of the algorithm
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data for the data station
+    col_name : str
+        The column to compute the mean and sample variance for. The column must be numeric.
+
+    Returns
+    -------
+    dict 
+        The mean, the number of observations and the sample variance for the data station.
+    """
+
+    if not ptypes.is_numeric_dtype(df[col_name]):
+        error("Column must be numeric.")
+
+    ### Compute mean ----
+    info("Computing mean")
+    # Sum of the values
+    col_sum = df[col_name].sum()
+    # Count of observations
+    count = len(df)
+    # Mean
+    avg = col_sum/count
+
+    ### Compute sample variance (S) ----
+    info("Computing sample variance")
+    # Sum of Squared Deviations (SSD)
+    ssd = ((df[col_name].astype(float) - avg) ** 2).sum()
+    # Sample variance
+    S = ssd/(count - 1)
 
     # Return results to the vantage6 server.
-    # TODO make sure no privacy sensitive data is shared
-    return result.to_dict()
-
-# TODO Feel free to add more partial functions here.
+    return {"avg": float(avg), "count": float(count), "S": float(S)}
